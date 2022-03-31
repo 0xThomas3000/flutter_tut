8f9fb00bec02b5ea7da2d7fe8ba6cc3499e39df6
tuanit168 <tuanit168@gmail.com>
2022-03-31 17:36:09 +0700

    Setup Flutter tutorials workspace

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter tutorials',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Flutter tutorials')),
+        body: Container(
+          
+        ),
+      ),
+    );
+  }
+}
